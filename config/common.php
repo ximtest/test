40be0ca497a0655523ae304dddc4b7d1ce864bdc
Mikhail Vasilyev <m.vasilyev@likefifa.ru>
2015-04-03 16:33:44 +0400

    config

---
+++ config/common.php
@@ -0,0 +1,28 @@
+<?php
+
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', 'On');
+
+return [
+	'db'         => [
+		'host'     => 'localhost',
+		'user'     => 'mv_likefifa',
+		'password' => 'gtGfzt1',
+		'name'     => 'mvasilyev_likefifa',
+	],
+	'rootDir'    => __DIR__ . DIRECTORY_SEPARATOR . '..',
+	'urlManager' => [
+		''                              => 'site/index',
+		'topic/(\d+)'                   => 'topic/view',
+		'topic/add'                     => 'topic/add',
+		'topic/(\d+)/add'               => 'topic/add',
+		'topic/(\d+)/edit'              => 'topic/edit',
+		'topic/(\d+)/delete'            => 'topic/delete',
+		'topic/(\d+)/news/(\d+)'        => 'news/view',
+		'topic/(\d+)/news/add'          => 'news/add',
+		'topic/(\d+)/news/(\d+)/edit'   => 'news/edit',
+		'topic/(\d+)/news/(\d+)/delete' => 'news/delete',
+		'comment/add'                   => 'comment/add',
+		'comment/delete/(\d+)'          => 'comment/delete',
+	]
+];
